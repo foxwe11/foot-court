5445eddea5326c06f96ad63a5f5ae40f01c44441
Anton Varenik <anton.varenik@list.ru>
2019-10-30 22:45:24 +0300

    All Do Me

---
+++ Foot court/MainViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MainViewController.swift
+//  Foot court
+//
+//  Created by Admin on 28/10/2019.
+//  Copyright © 2019 Anton Varenik. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+
+   let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+
+    // MARK: - Table view data source
+
+   
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+
+        return cell
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
