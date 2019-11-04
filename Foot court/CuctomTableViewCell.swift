100a6d7dff2badba1076b469f61baa6285f0fd14
Anton Varenik <anton.varenik@list.ru>
2019-11-04 20:52:06 +0300

    Custom Table View Cell

---
+++ Foot court/CuctomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CuctomTableViewCell.swift
+//  Foot court
+//
+//  Created by Admin on 04/11/2019.
+//  Copyright © 2019 Anton Varenik. All rights reserved.
+//
+
+import UIKit
+
+class CuctomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+    
+
+}
